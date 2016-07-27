c9b5cd250081ae86d26ec12d34a8878d1e74f239
yingwei0831 <birneysky@163.com>
2016-07-28 00:03:52 +0800

    first commit

--- House-owner-assistant/ViewController.m
+++ House-owner-assistant/ViewController.m
@@ -7,33 +7,25 @@
 //
 
 #import "ViewController.h"
-#import <MapKit/MapKit.h>
 
-@interface ViewController () <MKMapViewDelegate>
-@property (weak, nonatomic) IBOutlet MKMapView *mapView;
 
-@property (nonatomic,strong) CLLocationManager* locationManager;
+@interface ViewController () 
+
 @end
 
 @implementation ViewController
 
 
-#pragma mark - *** Properties ***
-- (CLLocationManager*)locationManager
-{
-    if (!_locationManager) {
-        _locationManager = [[CLLocationManager alloc ] init];
-    }
-    return _locationManager;
-}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     //CLLocationManager* locationManager = [[CLLocationManager alloc] init];
-    //[self.locationManager requestWhenInUseAuthorization];
-    //self.mapView.delegate = self;
-    self.mapView.showsUserLocation = YES;
+}
+
+- (void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -41,19 +33,6 @@
     // Dispose of any resources that can be recreated.
 }
 
-#pragma mark - *** MKMapViewDelegate ***
--(void)mapView:(MKMapView *)mapView didUpdateUserLocation:(MKUserLocation *)userLocation
-{
-    CLLocationCoordinate2D loc = [userLocation coordinate];
-//    //显示到label上
-//    self.longitudeLabel.text = [NSString stringWithFormat:@"%f",loc.longitude];
-//    
-//    self.latitudeLabel.text = [NSString stringWithFormat:@"%f",loc.latitude];
-    
-    //放大地图到自身的经纬度位置。
-    MKCoordinateRegion region = MKCoordinateRegionMakeWithDistance(loc, 500, 500);
-    [self.mapView setRegion:region animated:YES];
-}
 
 
 @end
